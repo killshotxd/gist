e65146c8495fdb40e29e708e84085e9ce72017cb
killshotxd <hassanansari211@gmail.com>
2023-07-09 20:25:16 +0530

    Navigation fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Navbar from "./layouts/Navbar";
+import Create from "./pages/Create";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
       <Navbar />
     </>
--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -2,26 +2,37 @@ import { AiOutlineHome } from "react-icons/ai";
 import { FiSearch } from "react-icons/fi";
 import { IoMdCreate } from "react-icons/io";
 import { BiSolidUser } from "react-icons/bi";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className=" absolute flex left-[43%] right-[50%] bottom-6 w-min  z-10 bg-black glass rounded-lg shadow-md bac">
       <div className="w-4xl  mx-auto px-4">
         <div className="flex items-center justify-center h-16">
           <div className="flex justify-center items-center space-x-6 text-white">
-            <Link>
+            <span className=" cursor-pointer" onClick={() => navigate("/home")}>
               <AiOutlineHome size={25} />
-            </Link>
-            <Link>
+            </span>
+
+            <span className=" cursor-pointer">
               <FiSearch size={25} />
-            </Link>
-            <Link>
+            </span>
+
+            <span
+              className=" cursor-pointer"
+              onClick={() => navigate("/create")}
+            >
               <IoMdCreate size={25} />
-            </Link>
-            <Link>
+            </span>
+
+            <span
+              className=" cursor-pointer"
+              onClick={() => navigate("/profile")}
+            >
               <BiSolidUser size={25} />
-            </Link>
+            </span>
           </div>
         </div>
       </div>
